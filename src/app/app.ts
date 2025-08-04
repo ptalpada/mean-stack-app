191d45e9f2314c11a78a98e46e3f4d5b847e5527
Pruthvish T <pruthvish.talpada@gmail.com>
2025-08-04 00:10:00 -0500

    used dialog/modal from material

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostCreate } from './post-create/post-create';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PostCreate],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('mean-stack-app');
+  readonly dialog = inject(MatDialog);
+
+  openCreatePostDialog() {
+    this.dialog.open(PostCreate);
+  }
 }
--- src/app/post-create/post-create.ts
+++ src/app/post-create/post-create.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-post-create',
-  imports: [],
+  imports: [FormsModule, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule],
   templateUrl: './post-create.html',
   styleUrl: './post-create.scss'
 })
 export class PostCreate {
 
+  postText: string = '';
 
-onAddPost() {
-  alert('button clicked..')
-}
-
+  onAddPost() {
+    
+  }
 }
